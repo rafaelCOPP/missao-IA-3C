a3b507c36f5f7d46d91f14542fea7e118362dcdd
rafaelCOPP <128715716+rafaelCOPP@users.noreply.github.com>
2024-08-26 09:31:57 -0300

    .

---
+++ js/perguntas.js
@@ -0,0 +1,61 @@
+export const perguntas = [
+    {
+        enunciado: "Você assiste gameplay de algum youtuber ?",
+        alternativas: [
+            {
+                texto: "Sim",
+                afirmacao: [
+                    "Bom",
+                    "Muito Bom"
+                    ]
+            },
+            {
+                texto: "Não, eu sou um bosta",
+                afirmacao: [
+                    "Meu Deus",
+                    "kkkkkk"
+                    ]
+            }           
+            
+        ]
+    },
+    {
+        enunciado: "Você gosta de jogar jogos de terror ?",
+        alternativas: [
+            {
+                texto:"Sim",
+                afirmacao: [
+                    "bacana",
+                    "Tendi"
+                    ]
+            },
+            {
+                texto: "Não, tenho medo",
+                afirmacao: [
+                    "HMmmm",
+                    "QUanta boiolice"
+                    ]
+            }
+        ]
+    },
+    {
+        enunciado: "Você prefere que tipo de plataforma ?",
+        alternativas: [
+            {
+                texto:"Consoles",
+                afirmacao: [
+                    "Ih",
+                    "Se gosta de sofrer né"
+                    ]
+            },
+            {
+                texto:"Computador",
+                afirmacao: [
+                    "Legal",
+                    "Bacana"
+                    ]
+            }
+            
+        ]
+    },
+];
