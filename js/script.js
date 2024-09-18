4e7be0f2ea9900a37fe1fa77ffaff6c4e70442fa
rafaelCOPP <128715716+rafaelCOPP@users.noreply.github.com>
2024-09-18 07:39:11 -0300

    .

--- js/script.js
+++ js/script.js
@@ -8,11 +8,25 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente = document.querySelector(".novamente-btn");
+const botaoIniciar = document.querySelector(".iniciar-btn");
+const telaInicial = document.querySelector(".tela-inicial");
 
 
 let atual = 0; 
 let perguntaAtual;
 let historiaFinal = "";
+
+botaoIniciar.addEventListener('click', iniciaJogo);
+
+function iniciaJogo(){
+    atual = 0;
+    historiaFinal = "";
+    telaInicial.style.display = none;
+    caixaPerguntas.classList.remove("mostrar"); 
+    caixaAlternativas.classList.remove("mostrar");
+    caixaResultado.classList.remove("mostrar");
+    mostraPergunta();
+}
 
 function mostraPergunta() {
     if(atual >= perguntas.length){
@@ -65,6 +79,5 @@ function substituiNome(){
         pergunta.enunciado = pergunta.enunciado.replace(/você/g, nome);
     }
 }
-substituiNome();
 
-mostraPergunta();
+substituiNome();
