4bb8f341496ad6993f701855253a87d418a28997
rafaelCOPP <128715716+rafaelCOPP@users.noreply.github.com>
2024-10-16 07:23:45 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -59,7 +59,7 @@ function respostaSelecionada(opcaoSelecionada){
 }
 
 function mostraResultado(){
-    caixaPerguntas.textContent = `Em 2049, ${nome}`;
+    caixaPerguntas.textContent = `Após terminar o jogo, ${nome}`;
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
     caixaResultado.classList.add("mostrar"); 
